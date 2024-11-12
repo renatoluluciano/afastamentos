17d4d97a87254eb349375c18347282e03022a6f3
renatoluluciano <renatolucianosilva@gmail.com>
2024-11-12 09:40:09 -0300

    criacao projeto afastamentos

---
+++ Gerenciador-de-Afastamentos/src/main/java/com/afastamentos/GerenciadorDeAfastamentosApplication.java
@@ -0,0 +1,13 @@
+package com.afastamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeAfastamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeAfastamentosApplication.class, args);
+	}
+
+}
---
+++ Gerenciador-de-Afastamentos/src/test/java/com/afastamentos/GerenciadorDeAfastamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afastamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeAfastamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
